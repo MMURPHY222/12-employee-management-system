ad6c1c1266527f613cb230c894853f730a53f00a
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-17 22:44:25 -0700

    add employee functionality

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: process.env.port || 3306,
+    port: process.env.PORT || 3306,
     user: 'root',
     password: 'mysql',
-    // insert database name
     database: 'employeeTracker_DB',
   });
 
@@ -21,32 +20,86 @@ bigList = [
     }
 ];
 
+addEmployeeQs = [
+    {
+        type: "input",
+        message: "What is the employee's first name?",
+        name:"employeeFirst"
+    },
+    {
+        type: "input",
+        message: "What is the employee's last name?",
+        name:"employeeLast"
+    },
+    {
+        type: "list",
+        message: "What is the employee's role?",
+        name:"employeeRole",
+        choices:["Sales lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"],
+    },
+    // {
+    //     type: "list",
+    //     message: "Who is the employee's manager?",
+    //     name:"employeeManager",
+    //     choices:["lists all managers"],
+    // },
+];
+
 const start = () => {
     inquirer
         .prompt(bigList)
         .then((answer) => {
-            if(answer === "View All Employees"){
-                // TODO: show all employees
-            } else if(answer === "View All Employees by Department") {
-                // TODO: prompt for which department - Engineering, Finance, Legal, Sales
-                // TODO: show chart for selected department
-            } else if(answer === "View All Employees by Manager") {
-                // TODO: prompt to choose manager, lists all managers
-                // TODO: chart of returned employees with chosen manager
-            } else if(answer === "Add Employee") {
-                // TODO: prompt employee questions
-            } else if (answer === "Remove Employee") {
-                // TODO: prompt list of all employees, chosen one gets removed
-                // console log (EMployee removed)
-            } else if (answer === "Update Employee Role") {
-                // TODO: something
-            } else if (answer === "Update Employee Manager") {
-                // TODO: something
-            } else {
-                connection.end();
+            switch (answer.bigListChoice) {
+                case "View All Employees":
+                    // TODO: show all employees
+                    break;
+                case "View All Employees by Department":
+                    // TODO: prompt for which department - Engineering, Finance, Legal, Sales
+                    // TODO: show chart for selected department
+                    break;
+                case "View All Employees by Manager":
+                    // TODO: prompt to choose manager, lists all managers
+                    // TODO: chart of returned employees with chosen manager
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    // TODO: prompt list of all employees, chosen one gets removed
+                    // console log (EMployee removed)
+                    break;
+                case "Update Employee Role":
+                    // TODO: something
+                    break;
+                case "Update Employee Manager":
+                    // TODO: something
+                    break;
+                default:
+                    connection.end();
             }
+        }
         
-        });
+        );
+};
+
+const addEmployee = () => {
+    inquirer
+        .prompt(addEmployeeQs)
+        .then((answer) => {
+            connection.query(
+                `INSERT INTO employee SET ?`,
+                {
+                    first_name: answer.employeeFirst,
+                    last_name: answer.employeeLast,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Employee Created!');
+                    // re-prompt the user 
+                    start();
+                }
+            );
+        })
 };
 
 
