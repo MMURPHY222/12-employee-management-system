b86a05f6019071763e470598417032f36064f0cf
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-17 21:51:50 -0700

    created mysql database and tables

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'mysql',
     // insert database name
-    database: '',
+    database: 'employeeTracker_DB',
   });
 
 bigList = [
@@ -19,10 +19,41 @@ bigList = [
         choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", 
         "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Quit"]
     }
-]
+];
+
+const start = () => {
+    inquirer
+        .prompt(bigList)
+        .then((answer) => {
+            if(answer === "View All Employees"){
+                // TODO: show all employees
+            } else if(answer === "View All Employees by Department") {
+                // TODO: prompt for which department - Engineering, Finance, Legal, Sales
+                // TODO: show chart for selected department
+            } else if(answer === "View All Employees by Manager") {
+                // TODO: prompt to choose manager, lists all managers
+                // TODO: chart of returned employees with chosen manager
+            } else if(answer === "Add Employee") {
+                // TODO: prompt employee questions
+            } else if (answer === "Remove Employee") {
+                // TODO: prompt list of all employees, chosen one gets removed
+                // console log (EMployee removed)
+            } else if (answer === "Update Employee Role") {
+                // TODO: something
+            } else if (answer === "Update Employee Manager") {
+                // TODO: something
+            } else {
+                connection.end();
+            }
+        
+        });
+};
+
+
 
 connection.connect((err) => {
     if (err) throw err;
+
     // run the start function after the connection is made to prompt the user
     start();
   });
--- seed.sql
+++ seed.sql
@@ -2,3 +2,29 @@ DROP DATABASE IF EXISTS employeeTracker_DB;
 CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary INT default 0,
+    departmentID INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(departmentID) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    manager_id INT defualt null
+);
+
